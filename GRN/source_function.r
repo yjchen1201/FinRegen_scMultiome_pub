1763b898e970033a512fc49156f711e52024e59e
yujiechen <37003579+yjchen1201@users.noreply.github.com>
2024-10-23 23:22:03 -0700

    Update source_function.r

--- GRN/source_function.r
+++ GRN/source_function.r
@@ -2,9 +2,9 @@
 Get_corr_gene_motif <- function(motif_matrix,gene_matrix,Fish_All_motifs_table_short){
 
   ########
-  k = which(Matrix::rowSums(gene_matrix) ==0) # gene in row, cell in column
+  k = which(Matrix::rowSums(gene_matrix) < 10)
   if(length(k) > 1){
-    gene_matrix = gene_matrix[-k,] #gene 24380->20267 in mes
+    gene_matrix = gene_matrix[-k,] 
   }
   ######## clean cols #####
   cells = colnames(gene_matrix)[which(colnames(gene_matrix) %in% colnames(motif_matrix) == T)]
@@ -52,7 +52,6 @@ cor_functon <- function(x,motif_matrix_cl,gene_matrix_cl){
     return(Cor)
   }
 }
-
 
 # gene-gene correlation 
 sparse.cor3 <- function(x){
@@ -330,3 +329,164 @@ Process_peak_table_to_annotation <- function(ALL_peak_table){
   #####
   return(ALL_peak_table_cl4)
 }
+
+
+Get_all_need_Motif_tag <- function(x,cutoff=0.05){
+  ########
+  k1 = which(x$Gene_Motif_Cor > cutoff | x$Gene_Motif_Cor < -cutoff)
+  #########
+  x_cl = x[k1,]
+  #########
+  k3 = which(x_cl$Gene_Motif_Cor > cutoff)
+  k4 = which(x_cl$Gene_Motif_Cor < -cutoff)
+  x_cl$class = 'Unknown'
+  x_cl$class[k3] = 'pos'
+  x_cl$class[k4] = 'neg'
+  #########
+  print(length(x_cl$ID[!duplicated(x_cl$ID)]))
+  x_cl[which(x_cl$Gene == 'ascl1a'),]
+  return(x_cl)
+}
+
+Get_all_need_Peak_tag <- function(x1,x2,x3,x4){
+  all_tab = rbind(x1,x2,x3,x4)
+  ######
+  all_peak = all_tab$Peak
+  all_peak = all_peak[!duplicated(all_peak)]
+  ######
+  return(all_peak)
+}
+
+
+Match_motifs <- function(Peak_with_tag,Motif_with_tag,Fish_combined){
+  ############
+  Peak_with_tag_1 = Merge_peaks(Peak_with_tag,extend=150)
+  ############
+  Peak_with_tag_2 = Merge_GRlist(Peak_with_tag_1)
+  ############ filter the Fish_combined #####
+  k = which(names(Fish_combined) %in% Motif_with_tag$ID == T)
+  Fish_combined_cl = Fish_combined[k]
+  ############
+  library(parallel)
+  cl <- makeCluster(35)
+  ############
+  footprint_Motif_list = parLapply(cl,Fish_combined_cl,Find_Motif,Peak_with_tag_2=Peak_with_tag_2)
+  stopCluster(cl)
+  ############
+  head(names(footprint_Motif_list))
+  ###########
+  return(footprint_Motif_list)
+  ###########
+}
+
+Find_Motif <- function(x,Peak_with_tag_2){
+  library(motifmatchr)
+  library("BSgenome.Drerio.UCSC.danRer11")
+  footprint = matchMotifs(x,Peak_with_tag_2,genome = BSgenome.Drerio.UCSC.danRer11,out='positions',p.cutoff = 5e-05)
+  ######
+  footprint_tab = data.frame(footprint[[1]])
+  ######
+  return(footprint_tab)
+}
+
+
+
+
+Combined_all_things <- function(GG_network,PG_cor,TP_cor,Foot){
+  library(GenomicRanges)
+  ##### first filtr the GG_network #######
+  ##### keep 95% intervals ###############
+  GG_network = GG_network[order(GG_network$Score,decreasing=T),]
+  GG_cutoff = quantile(GG_network$Score,0.9)
+  #####
+  GG_network_cl = GG_network[which(GG_network$Score > GG_cutoff),]
+  #####
+  # filter abs(correlation) below 0.03
+  k1 = which(GG_network_cl$Corr > 0.03)
+  k2 = which(GG_network_cl$Corr < -0.03)
+  #####
+  GG_network_cl$Class = 'unknown'
+  GG_network_cl$Class[k1] = 'pos'
+  GG_network_cl$Class[k2] = 'neg'
+  #####
+  GG_network_clcl = GG_network_cl[which(GG_network_cl$Class != 'unknown'),]
+  GG_network_clcl$TF_Gene = paste0(GG_network_clcl$TF,'->',GG_network_clcl$Gene)
+  GG_network_clclcl = GG_network_clcl[,c('TF_Gene','Score','Corr')]
+  colnames(GG_network_clclcl) = c('TF_Gene','GG_Score','GG_Corr')
+  #####
+  ## head(GG_network_clcl[which(GG_network_clcl$TF == 'foxj1a'),])
+  #####
+  ##### Next filter the footprint #####
+  #####
+  k_foot <- which(c(Foot$left_delta + Foot$right_delta) > 0.1)
+  ######
+  Foot_cl = Foot[k_foot]
+  ######
+  ###### Next link footprint to Peaks !!! ###########
+  ######
+  Footprint_Peak_table = All_footprint_to_peaks(Foot_cl,PG_cor)
+  ###### Next merge the footpint peak to motif gene list !!! ##########
+  ######
+  colnames(TP_cor) = c("Motif","TF","TF_Motif_Cor","TAG") #modified from orignal code
+  TP_cor_cl = TP_cor[which(TP_cor$TF_Motif_Cor > 0.05 | TP_cor$TF_Motif_Cor < -0.05),]
+  TP_cor_cl = TP_cor_cl[,c("TF","Motif","TF_Motif_Cor")]
+  ###### Next we will merge big table ########
+  library(data.table)
+  library(dplyr)
+  Footprint_Peak_table = data.table(Footprint_Peak_table)
+  TP_cor_cl = data.table(TP_cor_cl)
+  merged_table <- inner_join(Footprint_Peak_table, TP_cor_cl, by = "Motif",relationship = "many-to-many")
+  ###### test merge ##### correct !! #
+  #Footprint_Peak_table = data.frame(Footprint_Peak_table)
+  #TP_cor_cl = data.frame(TP_cor_cl)
+  #merged_table_test <- merge(Footprint_Peak_table, TP_cor_cl)
+  #dim(merged_table_test)
+  ####### then next we will merge TF_gene corrections ######
+  merged_table$TF_Gene = paste0(merged_table$TF,"->",merged_table$Target)
+  ######
+  GG_network_clclcl <- data.table(GG_network_clclcl)
+  ######
+  merged_table2 = inner_join(merged_table, GG_network_clclcl, by = "TF_Gene")
+  ######
+  k_filter1 = which(merged_table2$TF_Motif_Cor > 0 & merged_table2$GG_Corr > 0)
+  k_filter2 = which(merged_table2$TF_Motif_Cor < 0 & merged_table2$GG_Corr < 0)
+  #######
+  merged_table3 = merged_table2[c(k_filter1,k_filter2),]
+  #######
+  print(dim(merged_table3))
+  return(merged_table3)
+}
+
+All_footprint_to_peaks <- function(footprint_GR,peak_table){
+  
+  peak_table_GR = GRanges(peak_table$Peak)
+  start(peak_table_GR) = start(peak_table_GR)-0  #original script is to extend 150 up and down,so total peak size will be 801bp for all peak, ours is different, since we didnt define peak region based on summit
+  end(peak_table_GR) = end(peak_table_GR)+0
+  #######
+  #######
+  ####### Next we extend the peak_table #######
+  ####### find the overlaps with peaks and footprint ####
+  ####### left is peak right is the footprint ###
+  countIndex = findOverlaps(peak_table_GR,footprint_GR)
+  ####### 
+  left_table = peak_table[queryHits(countIndex),]
+  #######
+  right_table = data.frame(footprint_GR)[subjectHits(countIndex),]
+  ####### Clean_left_table ########
+  left_table_cl = left_table[,c("Index","Peak","Class","PtoG_cor","Gene")]
+  colnames(left_table_cl) <- c("PtoG_Index","Peak","PtoG_Class","PtoG_cor","Target")
+  ### 
+  k = which(left_table_cl$PtoG_cor == 'ND')
+  left_table_cl$PtoG_cor[k] = 0
+  left_table_cl$PtoG_cor = round(as.numeric(left_table_cl$PtoG_cor),3)
+  ####### head(right_table)
+  right_table$footprint = paste0(right_table$seqnames,':',right_table$start,'-',right_table$end)
+  right_table_cl = right_table[,c('motif','footprint','mid_score','left_delta','right_delta')]
+  colnames(right_table_cl) <- c("Motif",'Footprint_region','Footprint_Mid',"Footprint_Left_del","Footprint_Right_del")
+  #######
+  merge_table = cbind(left_table_cl,right_table_cl)
+  #######
+  return(merge_table)
+}
+
+
